bdf05cfcf961f5b511860efa1fec9c82d83f7463
Ashwin Sinha <ash@ds.gg>
2015-11-12 17:02:22 -0800

    Adding waypoint functionality

---
+++ Assets/GridBehaviour/GridBehaviour.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GridBehaviour : MonoBehaviour
+{
+
+		int _unit;
+		/// <summary>
+		/// Get or set the unit of the grid.
+		/// Int.
+		/// </summary>
+		/// <value>The unit.</value>
+		public int unit
+		{
+			get
+			{
+				return this._unit;
+			}
+			set
+			{
+				this._unit = value;
+			}
+		}
+
+		GameObject _wayPoint;
+		/// <summary>
+		/// Gets or sets the way point.
+		/// Waypoint gameobject is used to debug 
+		/// the properties of a waypoint.
+		/// </summary>
+		/// <value>The way point.</value>
+		public GameObject wayPoint
+		{
+			get
+			{
+				return this._wayPoint;
+			}
+			set
+			{
+				this._wayPoint = value;
+			}
+		}
+	
+
+}
---
+++ Assets/GridBehaviour/GridWaypoints.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GridWaypoints : GridBehaviour 
+{
+	/// <summary>
+	/// Creates the waypoints.
+	/// Takes two inputs 0 from and to
+	/// </summary>
+	/// <param name="from">From.</param>
+	/// <param name="to">To.</param>
+	public void CreateWaypoints(Vector3 from, Vector3 to)
+	{
+
+	}
+
+	void CreatePoint(Vector3 pos)
+	{
+		Instantiate(wayPoint)
+	}
+
+}
